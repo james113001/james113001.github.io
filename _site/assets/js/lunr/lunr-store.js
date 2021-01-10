7db14226d08ebaf06231d49bc43c2794776b5877
james113001 <44733004+james113001@users.noreply.github.com>
2021-01-09 20:28:04 -0600

    Add files via upload

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Welcome To Jekyll",
+        "excerpt":"Jekyll requires blog post files to be named according to the following format: YEAR-MONTH-DAY-title.MARKUP Where YEAR is a four-digit number, MONTH and DAY are both two-digit numbers, and MARKUP is the file extension representing the format used in the file. After that, include the necessary front matter. Take a look...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/welcome-to-jekyll",
+        "teaser": null
+      }]
